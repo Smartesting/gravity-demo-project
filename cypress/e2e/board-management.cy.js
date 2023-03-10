a5f127d0db0a1d30003ee77477770c50e3b97495
Joan Racenet <joan.racenet@smartesting.com>
2023-03-10 14:37:45 +0100

    add some interactions in the test

--- cypress/e2e/board-management.cy.js
+++ cypress/e2e/board-management.cy.js
@@ -1,5 +1,8 @@
 describe('Board management', () => {
   it('passes', () => {
-    cy.visit('/')
+    cy.visit('/');
+    cy.get('[name="emailOrUsername"]').type('demo@demo.demo');
+    cy.get('[name="password"]').type('demo');
+    cy.get('button').click();
   })
 })
