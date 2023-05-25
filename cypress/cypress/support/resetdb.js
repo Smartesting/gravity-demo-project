30cb2d250d94db69540cf521dc0e758040046b48
Vincent Pretre <vincent.pretre@gmail.com>
2023-05-25 13:51:41 +0200

    Update location of Gravity related keys

--- cypress/cypress/support/resetdb.js
+++ cypress/cypress/support/resetdb.js
@@ -13,11 +13,9 @@ async function resetDB() {
   const updatedAt = new Date()
 
   await client.connect();
-  const drop = await client.query('DELETE FROM user_account WHERE email=$1', [email])
-  console.log({ drop })
-  const addUser = await client.query('insert into "user_account" ("created_at", "email", "is_admin", "name", "password", "subscribe_to_own_cards", "updated_at", "username") values ($1, $2, $3, $4, $5, $6, $7, $8)',
+  await client.query('DELETE FROM user_account WHERE email=$1', [email])
+  await client.query('insert into "user_account" ("created_at", "email", "is_admin", "name", "password", "subscribe_to_own_cards", "updated_at", "username") values ($1, $2, $3, $4, $5, $6, $7, $8)',
     [createdAt, email, isAdmin, name, password, subscribeToOwnCards, updatedAt, username])
-  console.log({ addUser })
 
   console.log('DB reset')
 }
